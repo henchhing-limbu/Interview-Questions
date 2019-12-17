c4a8f3ed515eab46df5135e694ae26d1fee0ccae
henchhing-limbu <henchhinglimbu@gmail.com>
2019-12-17 14:52:10 -0500

    Add solutions to creating queue using stack

---
+++ Stack-Queue/queue_using_stack.py
@@ -0,0 +1,45 @@
+"""
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+Notes:
+
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+"""
+
+class MyQueue:
+    def __init__(self):
+        self.enqueue = []
+        self.dequeue = []
+
+    def push(self, x):
+        self.enqueue.append(x)
+
+    def _move_enqueue_to_dequeue(self):
+        self.dequeue = self.enqueue[::-1]
+        self.enqueue.clear()
+
+    def pop(self):
+        if not self.dequeue:
+            self._move_enqueue_to_dequeue()    
+        return self.dequeue.pop()
+
+    def peek(self):
+        return self.dequeue[-1] if self.dequeue else self.enqueue[0]
+
+    def empty(self):
+        return not(self.dequeue or self.enqueue)
